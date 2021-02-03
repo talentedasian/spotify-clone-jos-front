ff53242b8861f593f370bff3029a28214bfc2358
almir james <lucenaalmirjames123@gmail.com>
2021-02-03 18:51:24 +0800

    added tracks using grids

--- src/components/searchItem/searchItem.js
+++ src/components/searchItem/searchItem.js
@@ -54,31 +54,43 @@ function SearchItem ( props ) {
                     <input className="search-bar" placeholder="Search" type="text" onKeyPress={e => setParameters(e.target.value)}/>
                 }
             </div>
-                <ul ref={itemContentOverflown} className="items-container"> 
-                    
-                    {   isItem && 
-
-                        item?.Artists?.map((artists,key) => {
-                       return  <li className="items-artist" key={key}>
-                            <Link to={`/artists/${artists.id}`} className="items-artist-link" onClick={() => setIdId(artists.id)}>
-                                <div className="items-artist-image" style={{
-                                    "background-image": `url(${artists.images[0]?.url})`
-                                        }} />
-                                <p className="item-names">{artists.name}</p>
-                                <p className="item-types">{artists.type}</p>
-                                </Link>
-                        </li>
-                    })
-                    }
+            <div>
+                {   isItem
+                        &&
+                    <ul ref={itemContentOverflown} className="items-artist-container"> 
+                        {item?.Artists?.map((artists,key) => {
+                            return  <li className="items-artist" key={key}>
+                                <Link to={`/artists/${artists.id}`} className="items-artist-link items-link" onClick={() => setIdId(artists.id)}>
+                                    <div className="items-artist-image" style={{
+                                        backgroundImage: `url(${artists.images[0]?.url})`
+                                    }} />
+                                    <p className="item-names">{artists.name}</p>
+                                    <p className="item-types">{artists.type}</p>
+                                    </Link>
+                            </li>
+                        })}
                     <div className="overflown-artist-icon-right-container">
                     {   isOverflown
                         &&
                         <BsBoxArrowInRight className="overflow-artist-icon-right" size="40px" />
                     }
-                </div>
+                    </div>
                 </ul>
-                <Artist id={id}/>
-                <Home />
+                }
+                {   isItem
+                        &&
+                    <ul className="items-tracks-container">
+                        {item?.Tracks?.map((tracks,key) => {
+                            return <li className="items-tracks" key={key}>
+                                <Link to={`/tracks/${tracks.id}`} className="items-artist-link items-link">
+                                    <img src={tracks.album?.images[0].url} className="items-tracks-image"/>
+                                    <p className="item-names">{tracks.name}</p>
+                                    <p className="item-types">{tracks.type}</p>
+                                </Link>
+                            </li>
+                        })}
+                </ul>}
+            </div>
         </div>
 
 
